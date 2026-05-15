17a67d2e60af48b5388ae3e8b46397d3b636a109
Ajay <i.ajaysharma1126@gmail.com>
2026-05-15 21:56:49 +0530

    Reversing an array via while loop

---
+++ src/Main/Java/Basic/Reverse_an_Array.java
@@ -0,0 +1,23 @@
+package Main.Java.Basic;
+
+public class Reverse_an_Array {
+
+    public static void main(String[] args) {
+        int[] arr= {9,8,7,6,5,4,3,2,1};
+
+        int left =0, right=arr.length-1;
+
+        while(left<right){
+            int temp = arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+
+            left++;
+            right--;
+        }
+        for(int k: arr){
+            System.out.print(k);
+        }
+    }
+
+}
