cdbc905ef3e0f08fc5dd45fc15d85529e5ab8f15
Ajay <i.ajaysharma1126@gmail.com>
2026-05-15 21:44:51 +0530

    Generating all subarrays with loops.

---
+++ src/Main/Java/Basic/Generate_all_SubArrays.java
@@ -0,0 +1,29 @@
+package Main.Java.Basic;
+
+import java.util.ArrayList;
+
+public class Generate_all_SubArrays {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        subArray(arr);
+
+    }
+
+    public static void subArray(ArrayList<Integer> arr){
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(arr.get(k)+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+}
