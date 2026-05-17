f693a3d980f135d51636a100dd1c764509206551
Ajay <i.ajaysharma1126@gmail.com>
2026-05-17 21:51:47 +0530

    Minimum cost to make array size 1 by removing larger of pair

---
+++ src/Main/Java/Basic/Minimum_cost_to_make_array_size_1.java
@@ -0,0 +1,19 @@
+package Main.Java.Basic;
+
+public class Minimum_cost_to_make_array_size_1 {
+    public static void main(String[] args) {
+        int[] arr = {2,3,6,4,7,10};
+        System.out.println(min_cost(arr));
+
+    }
+    public static int min_cost( int[] arr){
+        int min= arr[0];
+        int n = arr.length;
+        for(int i: arr){
+            if(min>i){
+                min=i;
+            }
+        }
+        return (n-1)*min;
+    }
+}
