df98e81bf86e3ffdf70eea136208d0c778cb5048
Ajay <i.ajaysharma1126@gmail.com>
2026-05-17 17:26:19 +0530

    Putting zeroes at end.

---
+++ src/Main/Java/Basic/Zeroes_to_the_end.java
@@ -0,0 +1,19 @@
+package Main.Java.Basic;
+
+public class Zeroes_to_the_end {
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,3,0,4,5,9};
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[count];
+                arr[count]=temp;
+                count++;
+            }
+        }
+        for(int j:arr){
+            System.out.print(j);
+        }
+    }
+}
