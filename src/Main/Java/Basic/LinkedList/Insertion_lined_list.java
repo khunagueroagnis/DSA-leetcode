040e4205baac7e14ff0dcea30c80c1df0374ac1c
Ajay <i.ajaysharma1126@gmail.com>
2026-06-11 10:07:31 +0530

    inserting node at front of linked list

---
+++ src/Main/Java/Basic/LinkedList/Insertion_lined_list.java
@@ -0,0 +1,36 @@
+package Main.Java.Basic.LinkedList;
+
+public class Insertion_lined_list {
+
+
+    public static void main(String[] args) {
+        Node head = new Node(2);
+        head.next = new Node(3);
+
+        int x = 1;
+        head = insertAtFront(head,x);
+        traverse(head);
+
+
+    }
+
+    public static Node insertAtFront(Node head, int x){
+        Node newnode = new Node(x);
+        newnode.next = head;
+        return newnode;
+    }
+
+
+
+    public static void traverse(Node head){
+        if(head == null){
+            System.out.println();
+        }
+        System.out.print(head.data);
+        if(head.next!= null) {
+            System.out.print("->");
+            traverse(head.next);
+        }
+    }
+
+}
