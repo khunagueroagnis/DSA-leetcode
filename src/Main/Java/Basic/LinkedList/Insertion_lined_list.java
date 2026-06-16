82633816c01075078d94f1b5150f47061133cabf
Ajay <i.ajaysharma1126@gmail.com>
2026-06-16 08:59:40 +0530

    insertion at front, back and specific postiion within a linked list

--- src/Main/Java/Basic/LinkedList/Insertion_lined_list.java
+++ src/Main/Java/Basic/LinkedList/Insertion_lined_list.java
@@ -8,7 +8,11 @@ public class Insertion_lined_list {
         head.next = new Node(3);
 
         int x = 1;
+        int k = 4;
+        int m = 56;
         head = insertAtFront(head,x);
+        head = insertAtBack(head,k);
+        head = insertAtSpecificLocation(head,2,m);
         traverse(head);
 
 
@@ -20,7 +24,44 @@ public class Insertion_lined_list {
         return newnode;
     }
 
-    //have to do insertion at back and middle
+    public static Node insertAtBack(Node head, int x){
+        Node newnode = new Node(x);
+        if(head==null){
+            return newnode;
+        }
+        Node last = head;
+
+        while(last.next!=null){
+            last=last.next;
+        }
+        last.next= newnode;
+
+        return head;
+    }
+
+    // middle
+
+    public static Node insertAtSpecificLocation(Node head,int pos, int val){
+       Node newnode = new Node(val);
+        if(head==null){
+           return newnode;
+       }
+        if(pos==1){
+            newnode.next=head;
+        }
+
+        Node curr = head;
+        for(int i=1; i<pos-1 && curr!=null;i++){
+            curr = curr.next;
+        }
+        if(curr==null){
+            return head;
+        }
+        newnode.next = curr.next;
+        curr.next=newnode;
+
+        return head;
+    }
 
     public static void traverse(Node head){
         if(head == null){
