32c0b7d157f033e670fbdc8e3200aa63c15ccd5c
Ajay <i.ajaysharma1126@gmail.com>
2026-06-29 07:57:01 +0530

    doubly linked list creation and traversal from front and back

---
+++ src/Main/Java/Basic/LinkedList/Noda.java
@@ -0,0 +1,12 @@
+package Main.Java.Basic.LinkedList;
+
+public class Noda {
+    int data;
+    Noda next;
+    Noda prev;
+
+    Noda(int data){
+        this.data=data;
+        this.next=this.prev=null;
+    }
+}
---
+++ src/Main/Java/Basic/LinkedList/Traverse_Doubly_linked_list.java
@@ -0,0 +1,49 @@
+package Main.Java.Basic.LinkedList;
+
+public class Traverse_Doubly_linked_list {
+
+    public static void main(String[] args) {
+        Noda head = new Noda(10);
+
+        head.next = new Noda(20);
+        head.next.prev = head;
+
+        head.next.next = new Noda(30);
+        head.next.next.prev = head.next;
+
+        head.next.next.next = new Noda(40);
+        head.next.next.next.prev =  head.next.next;
+
+        backward_traversal(head.next.next.next);
+
+    }
+
+
+    public static void forward_traverse(Noda head){
+        Noda curr = head;
+
+        if(curr==null){
+            return;
+        }
+        System.out.print(curr.data +" ");
+        forward_traverse(curr.next);
+
+        }
+
+    public static void backward_traversal(Noda tail) {
+    Noda curr = tail;
+
+    if(curr == null){
+        return;
+    }
+    while(curr!=null){
+        System.out.print(curr.data + " ");
+
+        curr = curr.prev;
+    }
+
+    }
+
+
+    }
+
