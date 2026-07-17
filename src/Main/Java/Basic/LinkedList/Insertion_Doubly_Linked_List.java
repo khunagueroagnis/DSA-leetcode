106c8156ca66d8960baaf870d2f4fa6304ea99cb
Ajay <i.ajaysharma1126@gmail.com>
2026-07-17 19:20:44 +0530

    doubly linked list insertion at end and at certain postion

--- src/Main/Java/Basic/LinkedList/Insertion_Doubly_Linked_List.java
+++ src/Main/Java/Basic/LinkedList/Insertion_Doubly_Linked_List.java
@@ -4,18 +4,21 @@ public class Insertion_Doubly_Linked_List {
     public static void main(String[] args) {
 
         Noda head = new Noda(2);
-        head.next = new Noda(3);
+        head.next = new Noda(4);
         head.next.prev = head;
-        head.next.next = new Noda(4);
+        head.next.next = new Noda(5);
         head.next.next.prev = head.next;
 
 
 
 
         head = InsertAtFront(head,1);
-        InsertAtEnd(head,5);
+        InsertAtEnd(head,6);
+        InsertAtPosition(head,3,3);
 
         Traverse_Doubly_linked_list traverseClass = new Traverse_Doubly_linked_list();
+
+
 
         traverseClass.forward_traverse(head);
 
@@ -43,6 +46,28 @@ public class Insertion_Doubly_Linked_List {
         newNode.prev = curr;
 
         return head;
+    }
+
+    public static Noda InsertAtPosition(Noda head, int newData, int pos){
+        Noda newnode = new Noda(newData);
+        if(head == null){
+            newnode = head;
+        }
+        Noda curr = head;
+        int count = 1;
+
+        while(count < pos-1){
+            curr = curr.next;
+            count++;
+        }
+        newnode.next = curr.next;
+        newnode.prev = curr;
+        curr.next = newnode;
+
+
+
+    return head;
+
     }
 
 
