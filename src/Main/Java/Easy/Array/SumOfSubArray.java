625c3e1a6ed5d9cc2eb1db9a78e3827a1160e806
Ajay <i.ajaysharma1126@gmail.com>
2026-08-28 10:46:48 +0530

    sum of subarray o(n^2) instead of o(n^3)

--- src/Main/Java/Easy/Array/SumOfSubArray.java
+++ src/Main/Java/Easy/Array/SumOfSubArray.java
@@ -6,11 +6,12 @@ public class SumOfSubArray {
         int [] arr = {1,2,3,4,5,6};
         int sum =0;
         for(int i=0; i< arr.length; i++){
+            int temp=0;
             for(int j=i; j<arr.length;j++){
-                for(int k=i; k<=j; k++){
-                    sum += k;
-                }
+               temp+= arr[j];
+                sum += temp;
             }
+
         }
         System.out.println(sum);
 
