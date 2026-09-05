64a625de2e43284fbbda66314e3c55617d6ce9e6
Ajay <i.ajaysharma1126@gmail.com>
2026-09-05 19:28:53 +0530

    stock buy and sell

---
+++ src/Main/Java/Easy/Array/StockBuyAndSell.java
@@ -0,0 +1,24 @@
+package Main.Java.Easy.Array;
+
+public class StockBuyAndSell {
+    public static void main(String[] args) {
+
+        int arr[] = {100,180,260,310,40,535,695};
+
+        System.out.println(maxProfit(arr));
+    }
+
+    public static int maxProfit(int arr[]){
+        int profit =0;
+        for(int i=1; i<arr.length;i++){
+            if(arr[i]>arr[i-1]){
+                profit += arr[i]-arr[i-1];
+            }
+            else{
+                continue;
+            }
+        }
+        return profit;
+    }
+
+}
